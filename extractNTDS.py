f2ae6d16c089fcded2a833fee12b1612a3902cd6
Khalil Yahyaoui <yahyaoui.khalil@insat.u-carthage.tn>
2024-01-09 11:18:18 +0100

    added UAC handling

--- extractNTDS.py
+++ extractNTDS.py
@@ -3,6 +3,32 @@ import csv,sys
 
 csv.field_size_limit(sys.maxsize)
 
+USER_ACCOUNT_CONTROL = {
+    "SCRIPT": 0x0001,
+    "ACCOUNTDISABLE": 0x0002,
+    "HOMEDIR_REQUIRED": 0x0008,
+    "LOCKOUT": 0x0010,
+    "PASSWD_NOTREQD": 0x0020,
+    "PASSWD_CANT_CHANGE": 0x0040,
+    "ENCRYPTED_TEXT_PWD_ALLOWED": 0x0080,
+    "TEMP_DUPLICATE_ACCOUNT": 0x0100,
+    "NORMAL_ACCOUNT": 0x0200,
+    "INTERDOMAIN_TRUST_ACCOUNT": 0x0800,
+    "WORKSTATION_TRUST_ACCOUNT": 0x1000,
+    "SERVER_TRUST_ACCOUNT": 0x2000,
+    "DONT_EXPIRE_PASSWORD": 0x10000,
+    "MNS_LOGON_ACCOUNT": 0x20000,
+    "SMARTCARD_REQUIRED": 0x40000,
+    "TRUSTED_FOR_DELEGATION": 0x80000,
+    "NOT_DELEGATED": 0x100000,
+    "USE_DES_KEY_ONLY": 0x200000,
+    "DONT_REQ_PREAUTH": 0x400000,
+    "PASSWORD_EXPIRED": 0x800000,
+    "TRUSTED_TO_AUTH_FOR_DELEGATION": 0x1000000,
+    "PARTIAL_SECRETS_ACCOUNT": 0x04000000,
+}
+
+
 def saveToCSV(infos,type,ntds_file):
     headers = ['samaccountname'] + list(next(iter(infos.values())).keys())
 
@@ -35,6 +61,13 @@ def parseRecord(record,fields):
                 if attribute is not None :
                     if isinstance(attribute,list):
                         attribute = ",".join(str(i) for i in attribute)
+                    if field == "userAccountControl":
+                        uac = []
+                        for key,val in USER_ACCOUNT_CONTROL.items():
+                            if val & int(attribute):
+                                uac.append(key)
+                        print(uac)
+                        attribute = "|".join(uac)
                     if isinstance(attribute,str) or isinstance(attribute,int):
                         attributes[field] = attribute
                 else : 
